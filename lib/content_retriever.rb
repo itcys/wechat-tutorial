0f33492ed101ba965c1cd0141e30f5a46effa68b
itcys <yangyuezhang@gmail.com>
2014-03-01 01:41:13 +0800

    Update content_retriever.rb

--- lib/content_retriever.rb
+++ lib/content_retriever.rb
@@ -6,10 +6,10 @@ class ContentRetriver
 		{
 			total: 1,
 			contents: [{
-				title: '简单几步成为微信公众平台开发者',
-				description: '该公众账户是为了演示如何成为微信平台的开发者，如果想要了解更详细的内容，请点击该消息。',
-				picture: 'https://mp.weixin.qq.com/cgi-bin/getheadimg?fakeid=3097029212&r=975188&token=1685872554',
-				url: 'http://linkedme.in/2013/09/25/turoial-to-be-wechat-public-account-developer/'}]
+				title: '微信是一种生活，微信是一种生意！',
+				description: '该账号为演示如何建设4S店工作互动账户',
+				picture: 'https://mp.weixin.qq.com/cgi-bin/getimgdata?token=405281282&msgid={msgid}&mode=large&source=file&fileId=10000076&ow=-1',
+				url: 'http://mp.weixin.qq.com/mp/appmsg/show?__biz=MjM5NDE5ODI0NQ==&appmsgid=10000077&itemidx=1&sign=fd04124b3f56fdc1938f80d9a0517dd8#wechat_redirect'}]
 		}
 	end
 end
